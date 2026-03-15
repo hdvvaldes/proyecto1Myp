b0973f2487be9e2c18b78b1096420259f3e92620
Hector Valdes <hdvvaldes@gmail.com>
2026-03-15 14:42:33 -0600

    refactor(Refactorization of main loop): It was starting to get really messy to manage. I needed to refactor in order to manage multi threads

--- server/serverChat/src/Server/App.hs
+++ server/serverChat/src/Server/App.hs
@@ -18,6 +18,8 @@ import Extra.ReaderT
 import Control.Monad.Reader.Class(MonadReader, asks)
 import Control.Monad.IO.Class(MonadIO, liftIO)
 import Server.ConnectionHandler(runConn)
+import Control.Concurrent.Async
+import Control.Concurrent.STM
 
 -- | The Application Monad.
 -- A wrapper around ReaderT that provides access to the application environment (Env)
@@ -34,21 +36,51 @@ newtype App a = App
 
 -- | The application environment.
 -- Currently holds only socket-related configuration.
-data Env = Env {
+data Env = Env{
   socketConfig :: SocketConfig
 }
 
 data SocketConfig = SocketConfig {
-  socketFamily ::Family,
+  socketFamily :: Family,
   socketType :: SocketType,
   socketProtoN :: ProtocolNumber,
   socketAddress :: SockAddr
 }
 
--- | Helper to run the App monad given an environment and an App computation.
+-- | Runs the app given the environment monad given an environment and an App computation.
 runApp :: Env -> App a -> IO a
 runApp e r = runReaderT (unApp r) e
 
+-- | Starts the server.
+-- 1. Builds the socket.
+-- 2. Binds and listens.
+-- 3. Enters the accept loop.
+runServer :: App ()
+runServer = do
+  sock <- buildSocket
+  liftIO $ putStrLn "Socket has been created"
+  SocketConfig{socketAddress} <- asks socketConfig
+  liftIO $ bind sock socketAddress
+  startListening sock
+  liftIO $ putStrLn "Listening at"
+  liftIO $ putStr (show socketAddress)
+  mainLoop sock
+
+-- | The main recursive loop that accepts new client connections.
+-- For each connection, it delegates handling to runConn.
+
+mainLoop :: Socket -> App()
+mainLoop sock = do
+  conn <- liftIO $ accept sock
+  liftIO $ do 
+    putStr "Connection Found at "
+    print $ snd conn
+    putStrLn ""
+  -- NOTE Hopefully forks and the app will still be able to receive new connections
+  _ <- liftIO $ async $ runConn conn
+  mainLoop sock
+
+------ Helper Function ------
 defaultSocket :: SocketConfig 
 defaultSocket = SocketConfig {
   socketFamily  = AF_INET, 
@@ -60,17 +92,6 @@ defaultSocket = SocketConfig {
     port = 8080
     host = tupleToHostAddress(127,0,0,1)
 
--- | Starts the server.
--- 1. Builds the socket.
--- 2. Binds and listens.
--- 3. Enters the accept loop.
-runServer :: App ()
-runServer = do
-  sock <- buildSocket
-  startSocket sock
-  mainLoop sock
-
------- Helper Function ------
 
 -- | Creates a new socket using the configuration in the environment.
 buildSocket :: App Socket
@@ -80,21 +101,15 @@ buildSocket = do
     socketType,
     socketProtoN} <- asks socketConfig
   liftIO $ socket socketFamily socketType socketProtoN
+ 
 
--- | Binds the socket to the configured address and starts listening for connections.
-startSocket :: Socket -> App ()
-startSocket sock = do 
+-- | Starts listening for connections.
+startListening :: Socket -> App ()
+startListening sock = do 
   SocketConfig{socketAddress} <- asks socketConfig
   liftIO $ bind sock socketAddress
-  let maxConn = 2
+  -- Maximum number of connections
+  let maxConn = 3
   liftIO $ listen sock maxConn
 
--- | The main recursive loop that accepts new client connections.
--- For each connection, it delegates handling to runConn.
-mainLoop :: Socket -> App()
-mainLoop sock = do
-  conn <- liftIO $ accept sock
-  liftIO $ runConn conn
-  liftIO $ putStrLn "connection found"
-  mainLoop sock
 
