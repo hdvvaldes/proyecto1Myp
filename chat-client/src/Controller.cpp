502df615eb143e3e3226dc573ed32f0983515eef
Hector Valdes <hdvvaldes@gmail.com>
2026-03-19 17:07:36 -0600

    bug(Makefile): Confussion between wildcards, added file testing entry point

---
+++ chat-client/src/Controller.cpp
@@ -0,0 +1,20 @@
+#include "Controller.hpp"
+#include <iostream>
+
+// ---------------------------------------------
+//  Constructor
+// ---------------------------------------------
+
+Controller::Controller()
+{
+
+}
+
+
+// ---------------------------------------------
+//  run – main loop
+// ---------------------------------------------
+
+void Controller::run() {
+  std::cout << "Controller. run"  << std::endl;
+}
