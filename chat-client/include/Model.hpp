968710e760c410ed4f6d484742bc10e957a0b3ff
Hector Valdes <hdvvaldes@gmail.com>
2026-03-19 21:35:41 -0600

    feature(interface model): Function to modify model state(update users, update status, etc) and make queries

--- chat-client/include/Model.hpp
+++ chat-client/include/Model.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <functional>
+#include <string>
+#include <map>
+#include <set>
 
 enum class UserStatus {
   ACTIVE,
@@ -14,14 +17,73 @@ enum class ConnectionState {
     IDENTIFIED       // Identified with a username
 };
 
+struct RoomInfo {
+    std::string roomname;
+    // username → status
+    std::map<std::string, UserStatus> users;  
+};
+
 class Model {
 public:
   // TODO Replace **const Model** for the correct data type
+  // TODO Investigate how to use callback
   using EventCallback = std::function<void(const Model)>;
   Model();
 
-private:
-    ConnectionState connectionState_ = ConnectionState::DISCONNECTED;
+  // -- State Modifiers -----
+  void setConnectionState(ConnectionState state);
+  void setUsername(const std::string& username);
+  void setUserStatus(UserStatus status);
 
+  // Users
+  void addUser(const std::string& username, UserStatus status = UserStatus::ACTIVE);
+  void removeUser(const std::string& username);
+  void updateUserStatus(const std::string& username, UserStatus status);
+  void setUserList(const std::map<std::string, UserStatus>& users);
+
+  // Rooms
+  void addRoom(const std::string& roomname);
+  void removeRoom(const std::string& roomname);
+  void addInvitation(const std::string& roomname);
+  void joinRoom(const std::string& roomname);
+  void leaveRoom(const std::string& roomname);
+  void setRoomUserList(const std::string& roomname,
+      const std::map<std::string, UserStatus>& users);
+  void userJoinedRoom(const std::string& roomname, const std::string& username);
+  void userLeftRoom(const std::string& roomname, const std::string& username);
+
+  // -- Getters ---------
+  // Const bc model is not being modified
+  ConnectionState getConnectionState() const 
+    { return connectionState_; }
+
+  const std::string& getUsername() const 
+    { return username_; }
+
+  UserStatus getMyStatus() const 
+    { return myStatus_; }
+
+  const std::map<std::string, UserStatus>& getUsers() const
+    { return users_; }
+
+  const std::map<std::string, RoomInfo>& getRooms() const
+    { return rooms_; }
+
+  bool isIdentified() const 
+    { return connectionState_ == ConnectionState::IDENTIFIED; }
+
+  bool isInRoom(const std::string& roomname) const;
+  bool isInvitedToRoom(const std::string& roomname) const;
+private:
+  ConnectionState connectionState_ = ConnectionState::DISCONNECTED;
+  std::string username_;
+  UserStatus myStatus_ = UserStatus::ACTIVE;
+   // All online users
+  std::map<std::string, UserStatus> users_;
+  // Rooms we have joined
+  std::map<std::string, RoomInfo> rooms_; 
+  // TODO Overkill set?
+  // Pending invitations
+  std::set<std::string> invitations_; 
 };
 
--- chat-client/src/Model.cpp
+++ chat-client/src/Model.cpp
@@ -1,27 +1,107 @@
-#pragma once 
+#include "Model.hpp"
 
-#include <functional>
+Model::Model() = default;
 
-enum class UserStatus {
-  ACTIVE,
-  AWAY,
-  BUSY
-};
+// -- State setters ----------
 
-enum class ConnectionState {
-    DISCONNECTED,
-    CONNECTED,       // TCP connected, not yet identified
-    IDENTIFIED       // Identified with a username
-};
+void Model::setConnectionState(ConnectionState state) {
+    connectionState_ = state;
+}
 
-class Model {
-public:
-  // TODO Replace **const Model** for the correct data type
-  using EventCallback = std::function<void(const Model)>;
-  Model();
+void Model::setUsername(const std::string& username) {
+    username_ = username;
+}
 
-private:
-    ConnectionState connectionState_ = ConnectionState::DISCONNECTED;
+void Model::setUserStatus(UserStatus status) {
+    myStatus_ = status;
+    auto it = users_.find(username_);
+    if (it != users_.end())
+      it->second = status;
+}
 
-};
+// Users
+
+void Model::addUser(const std::string& username, UserStatus status) {
+    users_[username] = status;
+}
+
+void Model::removeUser(const std::string& username) {
+    users_.erase(username);
+    // Also remove from all rooms
+    for (auto& [rname, rinfo] : rooms_)
+        rinfo.users.erase(username);
+}
+
+void Model::updateUserStatus(const std::string& username, UserStatus status) {
+    users_[username] = status;
+    // Propagate to rooms
+    for (auto& [rname, rinfo] : rooms_) {
+        auto it = rinfo.users.find(username);
+        if (it != rinfo.users.end())
+            it->second = status;
+    }
+}
+
+void Model::setUserList(const std::map<std::string, UserStatus>& users) {
+    users_ = users;
+}
+
+// Rooms
+
+void Model::addRoom(const std::string& roomname) {
+    rooms_[roomname] = RoomInfo{ roomname, {} };
+}
+
+void Model::removeRoom(const std::string& roomname) {
+    rooms_.erase(roomname);
+    invitations_.erase(roomname);
+}
+
+void Model::addInvitation(const std::string& roomname) {
+    invitations_.insert(roomname);
+}
+
+void Model::joinRoom(const std::string& roomname) {
+    invitations_.erase(roomname);
+    if (rooms_.find(roomname) == rooms_.end())
+        rooms_[roomname] = RoomInfo{ roomname, {} };
+    rooms_[roomname].users[username_] = myStatus_;
+}
+
+void Model::leaveRoom(const std::string& roomname) {
+    rooms_.erase(roomname);
+    invitations_.erase(roomname);
+}
+
+void Model::setRoomUserList(const std::string& roomname,
+                             const std::map<std::string, UserStatus>& users) {
+    rooms_[roomname].users = users;
+}
+
+void Model::userJoinedRoom(const std::string& roomname, const std::string& username) {
+    auto status = UserStatus::ACTIVE;
+    auto it = users_.find(username);
+    if (it != users_.end()) status = it->second;
+    rooms_[roomname].users[username] = status;
+}
+
+void Model::userLeftRoom(const std::string& roomname, const std::string& username) {
+    auto rit = rooms_.find(roomname);
+    if (rit != rooms_.end())
+        rit->second.users.erase(username);
+}
+
+
+// -- Queries ------------
+
+bool Model::isInRoom(const std::string& roomname) const {
+    return rooms_.find(roomname) != rooms_.end();
+}
+
+bool Model::isInvitedToRoom(const std::string& roomname) const {
+    return invitations_.find(roomname) != invitations_.end();
+}
+
+
+
 
