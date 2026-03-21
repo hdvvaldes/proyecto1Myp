ee70b9a53bf2b8d0e29c0c56571f36ba7621d705
Hector Valdes <hdvvaldes@gmail.com>
2026-03-20 19:00:19 -0600

    feature(logger): Logger in handleEnv

--- server/serverChat/src/Server/App.hs
+++ server/serverChat/src/Server/App.hs
@@ -14,6 +14,7 @@ import Network.Socket
 import Server.ConnectionHandler (runConn)
 import Server.Connection.Types (HandlerEnv(..))
 import Server.ServerState (newServerState)
+import qualified Data.Text as T
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM (atomically)
@@ -53,7 +54,11 @@ runServer config = do
   -- Creating Server State
   stateVar <- atomically newServerState 
   
-  let handleEnv = HandlerEnv { handlerClient = Nothing, serverState = stateVar }
+  let handleEnv = HandlerEnv 
+        { handlerClient = Nothing
+        , serverState = stateVar
+        , logger = \t -> serverLog (T.unpack t)
+        }
 
   --- ACCEPT LOOP ----
   acceptLoop handleEnv sock 
